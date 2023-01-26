c9f7f93df37355cff0d10cc6f9f73a653516f404
Alfred Rayo <ray22933@uvg.edu.gt>
2023-01-25 22:20:18 -0600

    Adding Unit Test for StackedArray class

---
+++ App_main/src/main/java/App_main/model/FileReader.java
@@ -0,0 +1,2 @@
+package App_main.model;public class FileReader {
+}
--- App_main/src/test/java/App_main/model/StackedArrayTest.java
+++ App_main/src/test/java/App_main/model/StackedArrayTest.java
@@ -1,34 +1,48 @@
 package App_main.model;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class StackedArrayTest {
 
-    @Before
-    public void setUp() throws Exception {
-        System.out.printf("owo");
+    @Test
+    public void sizeOfStackMustBeTwo() {
+        StackedArray<String> stack = new StackedArray<>();
+        stack.push("1");
+        stack.push("2");
+        assertEquals(stack.size(), 2);
     }
 
     @Test
-    public void push() {
+    public void popTwoAsLastElementAndSizeDecreasedToOne() {
+        StackedArray<String> stack = new StackedArray<>();
+        stack.push("1");
+        stack.push("2");
+        assertEquals(stack.pop(), "2");
+        assertEquals(stack.size(), 1);
     }
 
     @Test
-    public void pop() {
+    public void peekMustReturnSameAsPop() {
+        StackedArray<String> stack = new StackedArray<>();
+        stack.push("1");
+        stack.push("2");
+        assertEquals(stack.peek(), stack.pop());
     }
 
     @Test
-    public void peek() {
+    public void newStackMustBeEmpty() {
+        StackedArray<String> stack = new StackedArray<>();
+        assertEquals(stack.empty(), true);
     }
 
     @Test
-    public void empty() {
-    }
-
-    @Test
-    public void size() {
+    public void emptyStackMustThrowExceptionWhenPopEmpty(){
+        StackedArray<String> stack = new StackedArray<>();
+        assertThrows(EmptyStackException.class, stack::pop);
     }
 }
