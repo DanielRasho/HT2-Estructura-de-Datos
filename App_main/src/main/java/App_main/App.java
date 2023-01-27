c0152d68858c97cbc8f3969675c51009f38d5db0
DiegoLinares11 <lin221256@uvg.edu.gt>
2023-01-27 12:40:06 -0600

    comentarios

--- App_main/src/main/java/App_main/App.java
+++ App_main/src/main/java/App_main/App.java
@@ -1,14 +1,19 @@
 package App_main;
+
 /**
- * Hello world!
+ * Autores:
+ * 
+ * 
+ * 
+ * Fecha:
+ * Catedratico:
+ * Curso:
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        //System.out.println( "Hello World!" );
-        MostrarMenu mostrarMenu= new MostrarMenu();
+public class App {
+    public static void main(String[] args) {
+        // System.out.println( "Hello World!" );
+        MostrarMenu mostrarMenu = new MostrarMenu();
         mostrarMenu.MostrarPrograma();
     }
 }
