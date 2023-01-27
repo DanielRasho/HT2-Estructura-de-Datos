9dab12a120f389ec8bbe411f67b12a1867649508
angelargd8 <angelargd8@outlook.com>
2023-01-26 23:09:56 -0600

    arreglos

--- App_main/src/main/java/App_main/MostrarMenu.java
+++ App_main/src/main/java/App_main/MostrarMenu.java
@@ -15,8 +15,8 @@ public class MostrarMenu {
         
         
 
-        while (opcion!=8){
-            opcion= vista.SeleccioneMenu("\nBienvenido a su calculadora\nAntes de realizar la operación deseada asegurese que la expresión en el archivo este bien escrita\n\nIngrese una opción:\n1.Leer el archivo  \n2. Ingresar una expresión \n3.  \n4.  \n5. \n6. \n7. \n8. salir");
+        while (opcion!=4){
+            opcion= vista.SeleccioneMenu("\nBienvenido a su calculadora\nAntes de realizar la operación deseada asegurese que la expresión en el archivo este bien escrita\n\nIngrese una opción:\n1.Leer el archivo  \n2.Ingresar una expresión \n3.Calcular  \n4. salir");
 /////////////1. Leer el archivo//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             if (opcion==1){
                 controlador.leerArchivo();
@@ -33,31 +33,10 @@ public class MostrarMenu {
                     
 
                 }
-    /////////////4.  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-                else if (opcion==4){
-                    
-                    
-                }
-    //////////////5. ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-                else if (opcion==5){
-                    
-                }
-    //////////////6. /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-                else if (opcion==6){
-                    
-                }
-    /////////////7.  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                else if (opcion==7){
-                    
-
-                }
             /////salir//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             }catch (Exception e){
-                vista.validacion2();
+                vista.validacion();
             }
         }
 
